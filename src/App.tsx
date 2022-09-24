86217961b953cf5aff5f8782c5c573a30e989ca0
yugo uchiida <y.uchiida@nirneege.com>
2022-09-25 02:19:14 +0900

    implemented simple todo list app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,88 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { v4 as uuidv4 } from 'uuid';
 import './App.css'
 
+import { Container, Form, InputGroup, Button, Table } from 'react-bootstrap';
+
+interface todoItem {
+  id: string
+  title: string,
+  completed: boolean,
+};
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState('');
+  const [todoList, setTodoList] = useState<todoItem[]>([]);
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = e => {
+    e.preventDefault();
+    if (value === '') {
+      return;
+    }
+    addTodo(value);
+    setValue('');
+  };
+
+  const addTodo = (text: string) => {
+    const newTodo: todoItem = { id: uuidv4(), title: text, completed: false }
+    setTodoList(todoList => [...todoList, newTodo]);
+  };
+
+  const removeTodoItem = (id: string) => {
+    setTodoList(todoList.filter(item => item.id !== id));
+  }
+  const toggleCompleteTodoItem = (id: string) => {
+    setTodoList(todoList.map(item => {
+      if (item.id === id) {
+        item.completed = !item.completed;
+      }
+      return item;
+    }))
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <h1 className='mt-4'>Todo App</h1>
+        <Form onSubmit={handleSubmit}>
+          <InputGroup>
+            <Form.Control value={value} onChange={e => setValue(e.target.value)} />
+            <Button type='submit' variant='primary'>Add</Button>
+          </InputGroup>
+        </Form>
+      </Container>
+      <Container>
+        <Table>
+          <tbody>
+            <>
+              {todoList ?
+                todoList.map((item) => {
+                  return <tr key={item.id}>
+                    <td className='text-start'>
+                      {item.completed ?
+                        <s>{item.title}</s>
+                        :
+                        <>{item.title}</>
+                      }
+                    </td>
+                    <td className='text-end'>
+                      <Button
+                        variant={item.completed ? 'secondary' : 'success'}
+                        className='me-2'
+                        onClick={() => toggleCompleteTodoItem(item.id)}
+                      >
+                        {item.completed ? '完了' : '未完了'}
+                      </Button>
+                      <Button variant='danger' onClick={() => removeTodoItem(item.id)}>remove</Button>
+                    </td>
+                  </tr>
+                }) :
+                <p></p>
+              }
+            </>
+          </tbody>
+        </Table>
+      </Container>
     </div>
   )
 }
